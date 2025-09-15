16f6ab1051155e43040c2cc6bfb6b64722cffa5c
vuvoth <vu.voth@gmail.com>
2025-09-15 22:01:39 +0700

    chore: remove btc parser (#120)
    * remove btc_parser
    
    Signed-off-by: Vu Vo <vu.voth@gmail.com>
    
    * update deps
    
    Signed-off-by: Vu Vo <vu.voth@gmail.com>
    
    * fix ci
    
    Signed-off-by: Vu Vo <vu.voth@gmail.com>
    
    ---------
    
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- contrib/ci/run-move-tests.sh
+++ contrib/ci/run-move-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for name in "bitcoin_executor" "btc_parser" "nBTC" "nbtc_swap"; do
+for name in "bitcoin_executor" "nBTC" "nbtc_swap"; do
     echo testing $name
     cd $name && sui move build && sui move test && cd .. || exit 1
 done
