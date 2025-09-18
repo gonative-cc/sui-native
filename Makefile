112884f37db807b64ae6220b994e9ae8a2c56096
Robert Zaremba <robert@zaremba.ch>
2025-09-18 11:31:40 +0200

    fix makefiles
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
+.PHONY: setup-hooks lint-git
+
 setup-hooks:
 	@cd .git/hooks; ln -s -f ../../contrib/git-hooks/* ./
-	@pnpm install -g prettier @mysten/prettier-plugin-move
+	@bun install -g prettier @mysten/prettier-plugin-move
 
-# add license header to every source file
-add-license:
-	@awk -i inplace 'FNR==1 && !/SPDX-License-Identifier/ {print "// SPDX-License-Identifier: MPL-2.0\n"}1' */sources/*.move */tests/*.move
-# with reuse tool:
-# docker run --rm --volume $(pwd):/data fsfe/reuse annotate --license MPL-2.0  */tests/*.move */sources/*.move  -s cpp
+.git/hooks/pre-commit: setup
 
 
 # used as pre-commit
@@ -16,20 +14,33 @@ lint-git:
 
 ############
 
-# All immediate subdirectories
-SUBDIRS := $(wildcard */)
+# All immediate subdirectories inside /packages
+SUBDIRS := $(wildcard packages/*/)
 
 # Filter subdirectories that contain a Move.toml file
 MOVE_SUBDIRS := $(foreach dir,$(SUBDIRS),$(if $(wildcard $(dir)Move.toml),$(dir)))
+MAKE_SUBDIRS := $(foreach dir,$(SUBDIRS),$(if $(wildcard $(dir)Makefile),$(dir)))
 
+format-move:
+	@bun run format:move-all
 
 build-all:
 	@for dir in $(MOVE_SUBDIRS); do \
-		echo "==> building $$dir"; cd $$dir; sui move build; \
+		echo "==> building $$dir"; cd $$dir; sui move build; cd -; \
 	done
 
 
 test-all:
 	@for dir in $(MOVE_SUBDIRS); do \
-		echo "==> testing $$dir"; sui move test; \
+		echo "==> testing $$dir"; sui move test; cd -; \
 	done
+
+# add license header to every source file
+add-license:
+	@for dir in $(MOVE_SUBDIRS); do \
+		cd $$dir; make add-license; cd -; \
+	done
+# with reuse tool:
+# docker run --rm --volume $(pwd):/data fsfe/reuse annotate --license MPL-2.0  */tests/*.move */sources/*.move  -s cpp
+
+.PHONY: add-license, build-all, test-all
--- Makefile-common.mk
+++ Makefile-common.mk
@@ -1,15 +1,4 @@
 
-setup-hooks:
-	@cd .git/hooks; ln -s -f ../../scripts/git-hooks/* ./
-
-.git/hooks/pre-commit: setup
-
-build: .git/hooks/pre-commit
-	@sui move build
-
-publish:
-	@sui client publish --skip-dependency-verification  --gas-budget 100000000
-
 # used as pre-commit
 lint-git:
 	@git diff --name-only --cached | grep  -E '\.md$$' | xargs -r markdownlint-cli2
@@ -28,14 +17,24 @@ lint-all:
 lint-fix-all:
 	@markdownlint-cli2 --fix **.md
 	@echo "Sui move lint will be fixed by manual"
-	@prettier-move --w .
+	@bun run format:move-all
 
-.PHONY: build setup
-.PHONY: lint-git lint lint-all lint-fix-all
+
+.PHONY: setup lint-git lint lint-all lint-fix-all
+
+
+# add license header to every source file
+add-license:
+	@awk -i inplace 'FNR==1 && !/SPDX-License-Identifier/ {print "// SPDX-License-Identifier: MPL-2.0\n"}1' sources/*.move tests/*.move
+.PHONY: add-license
+
 
 ###############################################################################
-##                                   Tests                                   ##
+##                              Build & Test                                 ##
 ###############################################################################
+
+build: .git/hooks/pre-commit
+	@sui move build
 
 test:
 	@sui move test
@@ -46,6 +45,18 @@ test-coverage:
 # sui move coverage
 
 .PHONY: test test-coverage
+
+###############################################################################
+##                                   Docs                                    ##
+###############################################################################
+# Variables for build output and module name
+BUILD_DIR := build
+PACKAGE_NAME := $(notdir $(CURDIR))
+gen-docs:
+	@sui move build --doc
+	@cp -r ./$(BUILD_DIR)/$(PACKAGE_NAME)/docs/$(PACKAGE_NAME)/* ./docs
+
+.PHONY: gen-docs
 
 ###############################################################################
 ##                                Infrastructure                             ##
