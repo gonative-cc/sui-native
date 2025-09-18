645c42173704b53124c74e8e3f23d7928a23a5dd
Robert Zaremba <robert@zaremba.ch>
2025-09-18 23:10:30 +0200

    trigger js
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- contrib/bitcoin-key-inspect.js
+++ contrib/bitcoin-key-inspect.js
@@ -8,10 +8,10 @@ const bip32 = BIP32Factory(ecc);
 
 // derivation paths using BIP 44 (derives from BIP 32)
 const mainnetBIP32 = {
-  p2wpkh: "m/84'/0'/0'/0/0", // segwit
-  p2sh: "m/49'/0'/0'/0/0", // nested segwit = p2sh__p2wpkh
-  p2tr: "m/86'/0'/0'/0/0", // taproot
-  legacy_p2pkh: "m/44'/0'/0'/0/0", // deprecated
+	p2wpkh: "m/84'/0'/0'/0/0", // segwit
+	p2sh: "m/49'/0'/0'/0/0", // nested segwit = p2sh__p2wpkh
+	p2tr: "m/86'/0'/0'/0/0", // taproot
+	legacy_p2pkh: "m/44'/0'/0'/0/0", // deprecated
 };
 // NOTE: apparently this is not used - the mainnet is also used in testnet!
 // const testnetBIP32 = {
@@ -22,11 +22,9 @@ const mainnetBIP32 = {
 // };
 
 function getProperty(key, obj, msg) {
-  if (!(key in obj))
-    throw new Error(
-      "Wrong" + msg + ": " + key + ", must be in: " + Object.keys(obj),
-    );
-  return obj[key];
+	if (!(key in obj))
+		throw new Error("Wrong" + msg + ": " + key + ", must be in: " + Object.keys(obj));
+	return obj[key];
 }
 
 /**
@@ -34,9 +32,9 @@ function getProperty(key, obj, msg) {
  * @returns {string} - hex string of a hash160 (sha256 + ripmed160).
  */
 function hash160Hex(input) {
-  let x = bitcoin.crypto.sha256(input);
-  x = bitcoin.crypto.ripemd160(x);
-  return Buffer.from(x).toString("hex");
+	let x = bitcoin.crypto.sha256(input);
+	x = bitcoin.crypto.ripemd160(x);
+	return Buffer.from(x).toString("hex");
 }
 
 /**
@@ -44,12 +42,12 @@ function hash160Hex(input) {
  * @returns {object} - bitcoinKey.
  */
 function keyFromMnemonic(mnemonic, keyType) {
-  const seed = bip39.mnemonicToSeedSync(mnemonic);
-  const rootKey = bip32.fromSeed(seed);
+	const seed = bip39.mnemonicToSeedSync(mnemonic);
+	const rootKey = bip32.fromSeed(seed);
 
-  const path = getProperty(keyType, mainnetBIP32, "key type");
+	const path = getProperty(keyType, mainnetBIP32, "key type");
 
-  return rootKey.derivePath(path);
+	return rootKey.derivePath(path);
 }
 
 /**
@@ -59,24 +57,20 @@ function keyFromMnemonic(mnemonic, keyType) {
  * @returns {string | null} - The P2WPKH address, or null on error.
  */
 function keyToP2WPKHAddress(bitcoinKey, keyConfig) {
-  const paymentConstructor = getProperty(
-    keyConfig.typ,
-    bitcoin.payments,
-    "key type",
-  );
-  const n = getProperty(keyConfig.network, bitcoin.networks, "key type");
+	const paymentConstructor = getProperty(keyConfig.typ, bitcoin.payments, "key type");
+	const n = getProperty(keyConfig.network, bitcoin.networks, "key type");
 
-  const p = paymentConstructor({
-    pubkey: Buffer.from(bitcoinKey.publicKey),
-    network: n,
-  });
+	const p = paymentConstructor({
+		pubkey: Buffer.from(bitcoinKey.publicKey),
+		network: n,
+	});
 
-  if (!p.address) {
-    console.error("Failed to generate P2WPKH address.");
-    return null;
-  }
+	if (!p.address) {
+		console.error("Failed to generate P2WPKH address.");
+		return null;
+	}
 
-  return p.address;
+	return p.address;
 }
 
 //
@@ -84,27 +78,20 @@ function keyToP2WPKHAddress(bitcoinKey, keyConfig) {
 //
 
 let keyConfig = {
-  network: "testnet", // "bitcoin" | "testnet" | "regtest"
-  typ: "p2wpkh", // "p2wpkh" | "p2sh" | "p2tr" | "legacy_p2pkh"
+	network: "testnet", // "bitcoin" | "testnet" | "regtest"
+	typ: "p2wpkh", // "p2wpkh" | "p2sh" | "p2tr" | "legacy_p2pkh"
 };
 
 let mnemonic =
-  "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
+	"abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
 
 let key = keyFromMnemonic(mnemonic, keyConfig.typ);
 
 // Wallet Import Format - a format for private key containing:
 // version, key, compression byte and checksum
 console.log("Bitcoin Private Key (WIF):", key.toWIF());
-console.log(
-  "Bitcoin Private Key (hex):",
-  Buffer.from(key.privateKey).toString("hex"),
-);
-console.log(
-  "Bitcoin Public Key (hex):",
-  Buffer.from(key.publicKey).toString("hex"),
-);
+console.log("Bitcoin Private Key (hex):", Buffer.from(key.privateKey).toString("hex"));
+console.log("Bitcoin Public Key (hex):", Buffer.from(key.publicKey).toString("hex"));
 // this is probably what we need in the move transaction
 console.log("Bitcoin Public Key hash160 (hex):", hash160Hex(key.publicKey));
-
 console.log("P2WPKH testnet address", keyToP2WPKHAddress(key, keyConfig));
