6dc314052d36e0a9c8d6a9d3130ce8dcf58f1478
Robert Zaremba <robert@zaremba.ch>
2025-09-18 21:52:59 +0200

    CI: reorganize formatting jobs (#125)
    * ci: format
    * add missing ci
    * format
    * update CI permissions
    
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- contrib/ci/run-move-tests.sh
+++ contrib/ci/run-move-tests.sh
@@ -2,5 +2,5 @@
 
 for name in "bitcoin_executor" "nBTC" "nbtc_swap"; do
     echo testing $name
-    cd $name && sui move build && sui move test && cd .. || exit 1
+    cd $name && sui move test && cd .. || exit 1
 done
