ed071e8bebac1e7b85164a25da61ef87255bd2fe
Vu Vo <vu.voth@gmail.com>
2025-11-28 19:05:29 +0700

    update
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- nBTC/scripts/initialization.ts
+++ nBTC/scripts/initialization.ts
@@ -4,85 +4,85 @@
 
 import { Transaction } from "@mysten/sui/transactions";
 import {
-  createIkaClient,
-  createSuiClient,
-  executeTransaction,
-  getDwalletMetadata,
-  mkSigner,
-  type Config,
+	createIkaClient,
+	createSuiClient,
+	executeTransaction,
+	getDwalletMetadata,
+	mkSigner,
+	type Config,
 } from "./common";
-import { fromHex, toHex } from "@mysten/sui/utils";
+import { fromHex } from "@mysten/sui/utils";
 
 import { getUTXOs } from "./btc-helper";
 import type { IkaClient } from "@ika.xyz/sdk";
 import type { SuiClient } from "@mysten/sui/client";
 
 export async function initialization(dwalletId: string, config: Config) {
-  const suiClient = createSuiClient();
-  const ikaClient = createIkaClient(suiClient);
-  await ikaClient.initialize();
-  const dWallet = await ikaClient.getDWalletInParticularState(
-    dwalletId,
-    "Active",
-  );
-  const dwalletCap = dWallet.dwallet_cap_id;
+	const suiClient = createSuiClient();
+	const ikaClient = createIkaClient(suiClient);
+	await ikaClient.initialize();
+	const dWallet = await ikaClient.getDWalletInParticularState(
+		dwalletId,
+		"Active",
+	);
+	const dwalletCap = dWallet.dwallet_cap_id;
 
-  let { publicKey, lockscript } = await getDwalletMetadata(dWallet);
+	let { publicKey, lockscript } = await getDwalletMetadata(dWallet);
 
-  let tx = new Transaction();
+	let tx = new Transaction();
 
-  // add dwallet to nbtc
-  tx.moveCall({
-    target: `${config.packageId}::nbtc::add_dwallet`,
-    arguments: [
-      tx.object(config.adminCap),
-      tx.object(config.nbtc),
-      tx.object(dwalletCap),
-      tx.pure.vector("u8", lockscript as Uint8Array),
-      tx.pure.vector("u8", publicKey),
-      tx.pure.vector("u8", dWallet.public_user_secret_key_share!),
-    ],
-  });
+	// add dwallet to nbtc
+	tx.moveCall({
+		target: `${config.packageId}::nbtc::add_dwallet`,
+		arguments: [
+			tx.object(config.adminCap),
+			tx.object(config.nbtc),
+			tx.object(dwalletCap),
+			tx.pure.vector("u8", lockscript as Uint8Array),
+			tx.pure.vector("u8", publicKey),
+			tx.pure.vector("u8", dWallet.public_user_secret_key_share!),
+		],
+	});
 
-  tx.moveCall({
-    target: `${config.packageId}::nbtc::set_active_dwallet`,
-    arguments: [
-      tx.object(config.adminCap),
-      tx.object(config.nbtc),
-      tx.pure.id(dwalletId),
-    ],
-  });
+	tx.moveCall({
+		target: `${config.packageId}::nbtc::set_active_dwallet`,
+		arguments: [
+			tx.object(config.adminCap),
+			tx.object(config.nbtc),
+			tx.pure.id(dwalletId),
+		],
+	});
 
-  await executeTransaction(suiClient, tx);
+	await executeTransaction(suiClient, tx);
 }
 
 // init nbtc state for testing,
 // TODO: this function should remove after have e2e tests
 export async function mint_nbtc_for_testing(
-  ikaClient: IkaClient,
-  suiClient: SuiClient,
-  dwalletId: string,
-  config: Config,
+	ikaClient: IkaClient,
+	suiClient: SuiClient,
+	dwalletId: string,
+	config: Config,
 ) {
-  const dWallet = await ikaClient.getDWalletInParticularState(
-    dwalletId,
-    "Active",
-  );
-  let { addr } = await getDwalletMetadata(dWallet);
+	const dWallet = await ikaClient.getDWalletInParticularState(
+		dwalletId,
+		"Active",
+	);
+	let { addr } = await getDwalletMetadata(dWallet);
 
-  let utxos = await getUTXOs(addr);
-  let tx = new Transaction();
-  for (let i = 0; i < utxos.length; i++) {
-    tx.moveCall({
-      target: `${config.packageId}::nbtc::mint_nbtc_with_admin`,
-      arguments: [
-        tx.object(config.adminCap),
-        tx.object(config.nbtc),
-        tx.pure.vector("u8", fromHex(utxos[i].txid).reverse()),
-        tx.pure.u32(utxos[i].vout),
-        tx.pure.u64(utxos[i].value),
-      ],
-    });
-  }
-  await executeTransaction(suiClient, tx);
+	let utxos = await getUTXOs(addr);
+	let tx = new Transaction();
+	for (let i = 0; i < utxos.length; i++) {
+		tx.moveCall({
+			target: `${config.packageId}::nbtc::mint_nbtc_with_admin`,
+			arguments: [
+				tx.object(config.adminCap),
+				tx.object(config.nbtc),
+				tx.pure.vector("u8", fromHex(utxos[i].txid).reverse()),
+				tx.pure.u32(utxos[i].vout),
+				tx.pure.u64(utxos[i].value),
+			],
+		});
+	}
+	await executeTransaction(suiClient, tx);
 }
