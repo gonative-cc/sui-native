4320b309ade086f97ddbc5d6d45dbb4e593f36cb
Vu Vo <vu.voth@gmail.com>
2025-12-16 18:07:06 +0700

    gen typescript from move code
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

---
+++ sdk/sui-codegen.config.ts
@@ -0,0 +1,23 @@
+import type { SuiCodegenConfig } from '@mysten/codegen';
+
+const config: SuiCodegenConfig = {
+	output: './src/generated',
+	generateSummaries: true,
+	prune: true,
+	packages: [
+		{
+			package: '@local-pkg/nbtc',
+			path: '../nBTC',
+		},
+		{
+			package: '@local-pkg/bitcoin_spv',
+			path: "../bitcoin_spv"
+		},
+		{
+			package: '@local-pkg/bitcoin_lib',
+			path: "../bitcoin_lib"
+		}
+	],
+};
+
+export default config;
