859498a5249ff573a6c0f7b74b160239fa64c95c
Vu Vo <vu.voth@gmail.com>
2026-01-15 15:38:40 +0700

    fix bitcoinjs
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- nBTC/scripts/common.ts
+++ nBTC/scripts/common.ts
@@ -19,7 +19,9 @@ import { Transaction } from "@mysten/sui/transactions";
 import "dotenv/config";
 
 import * as bitcoin from "bitcoinjs-lib";
+import * as tinysecp from 'tiny-secp256k1';
 
+bitcoin.initEccLib(tinysecp);
 const REGTEST = bitcoin.networks.regtest;
 
 export type Config = {
@@ -51,12 +53,12 @@ export function createSuiClient(packageId?: string) {
 		url: getFullnodeUrl("testnet"),
 		mvr: packageId
 			? {
-					overrides: {
-						packages: {
-							"@local-pkg/nbtc": packageId,
-						},
+				overrides: {
+					packages: {
+						"@local-pkg/nbtc": packageId,
 					},
-				}
+				},
+			}
 			: undefined,
 	});
 }
